48ead3921b7147bfec1a509d1f99b72aec432cea
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-10-26 18:26:45 +0900

    updated dummy config files

--- enju_news.gemspec
+++ enju_news.gemspec
@@ -24,5 +24,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "vcr"
   s.add_development_dependency "fakeweb"
   s.add_development_dependency "sunspot-rails-tester"
-  #s.add_development_dependency "enju_leaf", "~> 1.1.0.rc3"
+  #s.add_development_dependency "enju_leaf", "~> 1.1.0.rc6"
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -9,33 +9,33 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130412083556) do
+ActiveRecord::Schema.define(version: 20130412083556) do
 
-  create_table "accepts", :force => true do |t|
+  create_table "accepts", force: true do |t|
     t.integer  "basket_id"
     t.integer  "item_id"
     t.integer  "librarian_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "accepts", ["basket_id"], :name => "index_accepts_on_basket_id"
-  add_index "accepts", ["item_id"], :name => "index_accepts_on_item_id"
+  add_index "accepts", ["basket_id"], name: "index_accepts_on_basket_id"
+  add_index "accepts", ["item_id"], name: "index_accepts_on_item_id"
 
-  create_table "baskets", :force => true do |t|
+  create_table "baskets", force: true do |t|
     t.integer  "user_id"
     t.text     "note"
-    t.integer  "lock_version", :default => 0, :null => false
-    t.datetime "created_at",                  :null => false
-    t.datetime "updated_at",                  :null => false
+    t.integer  "lock_version", default: 0, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "baskets", ["user_id"], :name => "index_baskets_on_user_id"
+  add_index "baskets", ["user_id"], name: "index_baskets_on_user_id"
 
-  create_table "bookstores", :force => true do |t|
-    t.text     "name",             :null => false
+  create_table "bookstores", force: true do |t|
+    t.text     "name",             null: false
     t.string   "zip_code"
     t.text     "address"
     t.text     "note"
@@ -44,23 +44,23 @@ ActiveRecord::Schema.define(:version => 20130412083556) do
     t.string   "url"
     t.integer  "position"
     t.datetime "deleted_at"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "budget_types", :force => true do |t|
+  create_table "budget_types", force: true do |t|
     t.string   "name"
     t.text     "display_name"
     t.text     "note"
     t.integer  "position"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "libraries", :force => true do |t|
-    t.string   "name",                                   :null => false
+  create_table "libraries", force: true do |t|
+    t.string   "name",                                null: false
     t.text     "display_name"
-    t.string   "short_display_name",                     :null => false
+    t.string   "short_display_name",                  null: false
     t.string   "zip_code"
     t.text     "street"
     t.text     "locality"
@@ -69,181 +69,180 @@ ActiveRecord::Schema.define(:version => 20130412083556) do
     t.string   "telephone_number_2"
     t.string   "fax_number"
     t.text     "note"
-    t.integer  "call_number_rows",      :default => 1,   :null => false
-    t.string   "call_number_delimiter", :default => "|", :null => false
-    t.integer  "library_group_id",      :default => 1,   :null => false
-    t.integer  "users_count",           :default => 0,   :null => false
+    t.integer  "call_number_rows",      default: 1,   null: false
+    t.string   "call_number_delimiter", default: "|", null: false
+    t.integer  "library_group_id",      default: 1,   null: false
+    t.integer  "users_count",           default: 0,   null: false
     t.integer  "position"
     t.integer  "country_id"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "deleted_at"
+    t.text     "opening_hour"
     t.string   "isil"
     t.float    "latitude"
     t.float    "longitude"
-    t.text     "opening_hour"
   end
 
-  add_index "libraries", ["library_group_id"], :name => "index_libraries_on_library_group_id"
-  add_index "libraries", ["name"], :name => "index_libraries_on_name", :unique => true
+  add_index "libraries", ["library_group_id"], name: "index_libraries_on_library_group_id"
+  add_index "libraries", ["name"], name: "index_libraries_on_name", unique: true
 
-  create_table "library_groups", :force => true do |t|
-    t.string   "name",                                                 :null => false
+  create_table "library_groups", force: true do |t|
+    t.string   "name",                                              null: false
     t.text     "display_name"
-    t.string   "short_name",                                           :null => false
+    t.string   "short_name",                                        null: false
     t.string   "email"
     t.text     "my_networks"
-    t.boolean  "use_dsbl",       :default => false,                    :null => false
-    t.text     "dsbl_list"
     t.text     "login_banner"
     t.text     "note"
     t.integer  "country_id"
-    t.datetime "created_at",                                           :null => false
-    t.datetime "updated_at",                                           :null => false
-    t.string   "url",            :default => "http://localhost:3000/"
+    t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "admin_networks"
+    t.string   "url",            default: "http://localhost:3000/"
   end
 
-  add_index "library_groups", ["short_name"], :name => "index_library_groups_on_short_name"
+  add_index "library_groups", ["short_name"], name: "index_library_groups_on_short_name"
 
-  create_table "news_feeds", :force => true do |t|
-    t.integer  "library_group_id", :default => 1, :null => false
+  create_table "news_feeds", force: true do |t|
+    t.integer  "library_group_id", default: 1, null: false
     t.string   "title"
     t.string   "url"
     t.text     "body"
     t.integer  "position"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "news_posts", :force => true do |t|
+  create_table "news_posts", force: true do |t|
     t.text     "title"
     t.text     "body"
     t.integer  "user_id"
     t.datetime "start_date"
     t.datetime "end_date"
-    t.integer  "required_role_id", :default => 1,     :null => false
+    t.integer  "required_role_id", default: 1,     null: false
     t.text     "note"
     t.integer  "position"
-    t.boolean  "draft",            :default => false, :null => false
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.boolean  "draft",            default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "url"
   end
 
-  add_index "news_posts", ["user_id"], :name => "index_news_posts_on_user_id"
+  add_index "news_posts", ["user_id"], name: "index_news_posts_on_user_id"
 
-  create_table "request_status_types", :force => true do |t|
-    t.string   "name",         :null => false
+  create_table "request_status_types", force: true do |t|
+    t.string   "name",         null: false
     t.text     "display_name"
     t.text     "note"
     t.integer  "position"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "request_types", :force => true do |t|
-    t.string   "name",         :null => false
+  create_table "request_types", force: true do |t|
+    t.string   "name",         null: false
     t.text     "display_name"
     t.text     "note"
     t.integer  "position"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "roles", :force => true do |t|
+  create_table "roles", force: true do |t|
     t.string   "name"
     t.text     "display_name"
     t.text     "note"
     t.integer  "position"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "search_engines", :force => true do |t|
-    t.string   "name",             :null => false
+  create_table "search_engines", force: true do |t|
+    t.string   "name",             null: false
     t.text     "display_name"
-    t.string   "url",              :null => false
-    t.text     "base_url",         :null => false
-    t.text     "http_method",      :null => false
-    t.text     "query_param",      :null => false
+    t.string   "url",              null: false
+    t.text     "base_url",         null: false
+    t.text     "http_method",      null: false
+    t.text     "query_param",      null: false
     t.text     "additional_param"
     t.text     "note"
     t.integer  "position"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "shelves", :force => true do |t|
-    t.string   "name",                            :null => false
+  create_table "shelves", force: true do |t|
+    t.string   "name",                         null: false
     t.text     "display_name"
     t.text     "note"
-    t.integer  "library_id",   :default => 1,     :null => false
-    t.integer  "items_count",  :default => 0,     :null => false
+    t.integer  "library_id",   default: 1,     null: false
+    t.integer  "items_count",  default: 0,     null: false
     t.integer  "position"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "deleted_at"
-    t.boolean  "closed",       :default => false, :null => false
+    t.boolean  "closed",       default: false, null: false
   end
 
-  add_index "shelves", ["library_id"], :name => "index_shelves_on_library_id"
+  add_index "shelves", ["library_id"], name: "index_shelves_on_library_id"
 
-  create_table "subscribes", :force => true do |t|
-    t.integer  "subscription_id", :null => false
-    t.integer  "work_id",         :null => false
-    t.datetime "start_at",        :null => false
-    t.datetime "end_at",          :null => false
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+  create_table "subscribes", force: true do |t|
+    t.integer  "subscription_id", null: false
+    t.integer  "work_id",         null: false
+    t.datetime "start_at",        null: false
+    t.datetime "end_at",          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "subscribes", ["subscription_id"], :name => "index_subscribes_on_subscription_id"
-  add_index "subscribes", ["work_id"], :name => "index_subscribes_on_work_id"
+  add_index "subscribes", ["subscription_id"], name: "index_subscribes_on_subscription_id"
+  add_index "subscribes", ["work_id"], name: "index_subscribes_on_work_id"
 
-  create_table "subscriptions", :force => true do |t|
-    t.text     "title",                           :null => false
+  create_table "subscriptions", force: true do |t|
+    t.text     "title",                        null: false
     t.text     "note"
     t.integer  "user_id"
     t.integer  "order_list_id"
     t.datetime "deleted_at"
-    t.integer  "subscribes_count", :default => 0, :null => false
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.integer  "subscribes_count", default: 0, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "subscriptions", ["order_list_id"], :name => "index_subscriptions_on_order_list_id"
-  add_index "subscriptions", ["user_id"], :name => "index_subscriptions_on_user_id"
+  add_index "subscriptions", ["order_list_id"], name: "index_subscriptions_on_order_list_id"
+  add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id"
 
-  create_table "user_groups", :force => true do |t|
+  create_table "user_groups", force: true do |t|
     t.string   "name"
-    t.string   "string"
+    t.text     "string"
     t.text     "display_name"
     t.text     "note"
     t.integer  "position"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "deleted_at"
-    t.integer  "valid_period_for_new_user", :default => 0, :null => false
+    t.integer  "valid_period_for_new_user", default: 0, null: false
     t.datetime "expired_at"
   end
 
-  create_table "user_has_roles", :force => true do |t|
+  create_table "user_has_roles", force: true do |t|
     t.integer  "user_id"
     t.integer  "role_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -254,9 +253,9 @@ ActiveRecord::Schema.define(:version => 20130412083556) do
     t.string   "locale"
     t.datetime "deleted_at"
     t.datetime "expired_at"
-    t.integer  "library_id",             :default => 1,  :null => false
-    t.integer  "required_role_id",       :default => 1,  :null => false
-    t.integer  "user_group_id",          :default => 1,  :null => false
+    t.integer  "library_id",             default: 1,  null: false
+    t.integer  "required_role_id",       default: 1,  null: false
+    t.integer  "user_group_id",          default: 1,  null: false
     t.text     "note"
     t.text     "keyword_list"
     t.integer  "failed_attempts"
@@ -265,22 +264,22 @@ ActiveRecord::Schema.define(:version => 20130412083556) do
     t.datetime "confirmed_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
-  add_index "users", ["user_group_id"], :name => "index_users_on_user_group_id"
-  add_index "users", ["user_number"], :name => "index_users_on_user_number", :unique => true
-  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  add_index "users", ["user_group_id"], name: "index_users_on_user_group_id"
+  add_index "users", ["user_number"], name: "index_users_on_user_number", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
-  create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
   end
 
-  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
 end
