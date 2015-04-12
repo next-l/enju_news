a03d4225559f31ceb467aa31908836b1df0d12e4
Kosuke Tanabe <nabeta@fastmail.fm>
2015-04-12 20:16:44 +0900

    convert encoding to utf-8

--- app/models/news_feed.rb
+++ app/models/news_feed.rb
@@ -15,7 +15,7 @@ class NewsFeed < ActiveRecord::Base
 
   def fetch
     begin
-      feed = Faraday.get(url).body
+      feed = Faraday.get(url).body.force_encoding('UTF-8')
       if rss = RSS::Parser.parse(feed, false)
         self.body = feed
       end
