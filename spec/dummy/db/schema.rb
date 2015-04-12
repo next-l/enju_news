b00725f6c2f4d1e6956840d0a77107707e92d2c1
Kosuke Tanabe <nabeta@fastmail.fm>
2015-04-12 20:29:29 +0900

    fixed dummy migration files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811031145) do
+ActiveRecord::Schema.define(version: 20150304175658) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer  "basket_id"
@@ -262,14 +262,16 @@ ActiveRecord::Schema.define(version: 20140811031145) do
 
   create_table "user_export_files", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "user_export_file_name"
-    t.string   "user_export_content_type"
-    t.integer  "user_export_file_size"
-    t.datetime "user_export_updated_at"
     t.datetime "executed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_export_id"
+    t.string   "user_export_file_name"
+    t.integer  "user_export_size"
+    t.string   "user_export_content_type"
   end
+
+  add_index "user_export_files", ["user_export_id"], name: "index_user_export_files_on_user_export_id"
 
   create_table "user_groups", force: :cascade do |t|
     t.string   "name"
