bef8a4d9a064e22f23e8b236a71898f98c21d434
Kosuke Tanabe <nabeta@fastmail.fm>
2018-02-17 14:48:19 +0900

    add version to dummy migrations

--- spec/dummy/db/migrate/015_create_creates.rb
+++ spec/dummy/db/migrate/015_create_creates.rb
@@ -1,4 +1,4 @@
-class CreateCreates < ActiveRecord::Migration
+class CreateCreates < ActiveRecord::Migration[5.1]
   def change
     create_table :creates do |t|
       t.references :agent, :null => false
